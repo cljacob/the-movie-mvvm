aea65d5b75c0e5989a3040abc56ef3939396c816
jacobcardozo <jaccobcl@gmail.com>
2021-07-06 11:56:02 -0500

    init proyect android studio

---
+++ app/src/main/java/co/jacobcloldev/apps/themoviecl/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.jacobcloldev.apps.themoviecl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
